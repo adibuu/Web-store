f4dccb304fce3ae456ee5e4e9b2d7e522624440c
Adrian <adibu8@gmail.com>
2021-09-18 20:14:55 +0200

    Push final project

---
+++ models/product.js
@@ -0,0 +1,29 @@
+const moongose = require("mongoose");
+
+const Schema = moongose.Schema;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = moongose.model("Product", productSchema);
